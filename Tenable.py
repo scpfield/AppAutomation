4cbb71aa493ad11335378f2a34cca87b7d98eca1
scpfield <95513302+scpfield@users.noreply.github.com>
2023-04-13 12:43:15 -0700

    Add files via upload

--- Tenable.py
+++ Tenable.py
@@ -18,70 +18,6 @@ class TenableApp( WebApp ):
     def __init__( self, UseAppium = False ):
         super().__init__( UseAppium )        
     ...
-
-    def DumpResult( self, Result, Node=None ):
-        
-        print()
-        print('---------------------------------')
-        if Node!= None:
-            print(Node.ObjPath)
-            print(Node.XPath)
-        print('.................................')
-        
-        Pause()
-        
-        if Result:
-        
-            if isinstance(Result, list):
-            
-                for Idx, Item in enumerate(Result):
-                    
-                    print("LIST ITEM #", Idx)
-                    print("------------")
-                    if isinstance(Item, dict):
-                        
-                        print("DICT ITEM")
-                        print("------------")
-                        
-                        for Key, Value in Item.items():
-                            
-                            print(Key, '     ', str(Value))
-                            
-                            if Key != 'dispatchEvent':
-                                continue
-                            
-                            if isinstance(Value, list):
-                            
-                                for ValueItem in Value:
-                                    
-                                    if isinstance(ValueItem, dict):
-                                    
-                                        for Key2, Value2 in ValueItem.items():
-                                        
-                                            print(Key2, Value2)
-                                            
-                            print(str(Value))
-                            
-                            
-                    else:
-                        print(type(Item))
-                        print(str(Item))
-                        Pause("Next List Item")
-                
-                return
-                
-            if isinstance(Result, str):
-                print(Result, Decorate=False)
-                return
-            
-            
-            print()
-            print(type(Result))
-            print(str(Result))
-            return
-            
-        else:
-            print("No Result")
     
     def Logon( self ):
 
@@ -127,56 +63,64 @@ class TenableApp( WebApp ):
         else:
             return True
     
-    
-    def AddCredential(self, CredentialsButton ):
-    
-            CredentialsButton.Click()
-            
-            WaitFor = { 'Tag' : 'li', 'data-name' : 'Windows' }
-            
-            if not self.LoadPage( PageName = "CredentialsPage", WaitFor = WaitFor ):
-                print('Failed to load CredentialsPage');
-                return False
-                    
-            CredentialsPageRoot = self.CredentialsPage.PageElementTree
-            
-            WindowsButton   = ([  Element
-                                  for Element in CredentialsPageRoot.Descendants()
-                                  if isinstance( Element, app_elements.ClickableElement )
-                                  if getattr( Element, 'data-name' ) == 'Windows' ])[0]
-                                  
-            WindowsButton.Click()
-            
-            WaitFor = { 'Tag' : 'div', 'data-input-id' : 'start_server_service' }
-            
-            if not self.LoadPage( PageName = "WindowsPage", WaitFor = WaitFor ):
-                print('Failed to load WindowsPage');
-                return False
-                
-            WindowsPageRoot = self.WindowsPage.PageElementTree
-                
-            UsernameField = ([  Element
-                                for  Element  in  WindowsPageRoot.Descendants()
-                                if   isinstance( Element, app_elements.EditableElement )
-                                if   getattr( Element, 'aria-label' ) == 'Username' 
-                                for  Ancestor in  Element.Ancestors()
-                                if   getattr( Ancestor, 'data-parent-id' ) == 'auth_method'
-                                if   'hide' not in getattr( Ancestor, 'class' ) ])[0]
 
-            PasswordField = ([  Element                            
-                                for  Element  in  WindowsPageRoot.Descendants()
-                                if   isinstance( Element, app_elements.EditableElement )
-                                if   getattr( Element, 'aria-label' ) == 'Password' 
-                                for  Ancestor in  Element.Ancestors()
-                                if   getattr( Ancestor, 'data-parent-id' ) == 'auth_method'
-                                if   'hide' not in getattr( Ancestor, 'class' ) ])[0]
-                            
-            UsernameField.SendKeys('Administrator')
-            PasswordField.SendKeys('admin')
+    def CreateAllScans(self):
 
-            return True
-    
-    
+        HomePageRoot    = self.HomePage.PageElementTree
+        NewScanButton   = ([  Element
+                              for  Element  in  HomePageRoot.Descendants()
+                              if   isinstance( Element, app_elements.ClickableElement )
+                              if   getattr(    Element, 'id' ) == 'new-scan' ])[0]
+                              
+        if NewScanButton:
+            NewScanButton.Click()
+        else:
+            return False
+
+        WaitFor = { 'Tag' : 'div', 'class' : 'library' }
+        
+        if not self.LoadPage( PageName = "ScanTemplatesPage", WaitFor = WaitFor ):
+            print('Failed to load ScanTemplatesPage');
+            return False
+        
+        ScanTemplatesPageRoot  = self.ScanTemplatesPage.PageElementTree
+
+        Categories  = ([    Element
+                            for  Element in ScanTemplatesPageRoot.Descendants()
+                            if   hasattr( Element, 'data-category' )
+                            if   getattr( Element, 'class' ) == 'category-templates' ])
+        
+        for Category in Categories:
+        
+            CategoryName = getattr( Category, 'data-category' )
+            
+            print( f"Category: {CategoryName}" )
+            
+            CategoryItems = ([  ( Element, Child.Text )
+                                for  Element in Category.Descendants()
+                                for  Child in Element.Descendants()
+                                if   isinstance( Element, app_elements.ClickableElement )
+                                if   getattr( Element, 'data-category' ) == CategoryName
+                                if   ( { 'class' : 'banner' }, True ) not in Element
+                                if   getattr( Child, 'class' ) == 'title'
+                            ])
+            
+            
+            for CategoryElement, CategoryTitle in CategoryItems:
+            
+                print("Creating Scan for: " + CategoryTitle)
+                
+                if not ( self.CreateScan( CategoryElement, CategoryTitle )):
+                    return False
+                
+                time.sleep(3)
+                NewScanButton.Instance = None
+                NewScanButton.Click()
+            
+        
+        return True
+        
+
     
     def CreateScan(self, CategoryElement, CategoryTitle ):
     
@@ -256,70 +200,58 @@ class TenableApp( WebApp ):
         SaveButton.Click()
 
         return True
-        
-        
+                
     
-    def CreateAllScans(self):
+    
+    def AddCredential(self, CredentialsButton ):
+    
+            CredentialsButton.Click()
+            
+            WaitFor = { 'Tag' : 'li', 'data-name' : 'Windows' }
+            
+            if not self.LoadPage( PageName = "CredentialsPage", WaitFor = WaitFor ):
+                print('Failed to load CredentialsPage');
+                return False
+                    
+            CredentialsPageRoot = self.CredentialsPage.PageElementTree
+            
+            WindowsButton   = ([  Element
+                                  for Element in CredentialsPageRoot.Descendants()
+                                  if isinstance( Element, app_elements.ClickableElement )
+                                  if getattr( Element, 'data-name' ) == 'Windows' ])[0]
+                                  
+            WindowsButton.Click()
+            
+            WaitFor = { 'Tag' : 'div', 'data-input-id' : 'start_server_service' }
+            
+            if not self.LoadPage( PageName = "WindowsPage", WaitFor = WaitFor ):
+                print('Failed to load WindowsPage');
+                return False
+                
+            WindowsPageRoot = self.WindowsPage.PageElementTree
+                
+            UsernameField = ([  Element
+                                for  Element  in  WindowsPageRoot.Descendants()
+                                if   isinstance( Element, app_elements.EditableElement )
+                                if   getattr( Element, 'aria-label' ) == 'Username' 
+                                for  Ancestor in  Element.Ancestors()
+                                if   getattr( Ancestor, 'data-parent-id' ) == 'auth_method'
+                                if   'hide' not in getattr( Ancestor, 'class' ) ])[0]
 
-        HomePageRoot    = self.HomePage.PageElementTree
-        NewScanButton   = ([  Element
-                              for  Element  in  HomePageRoot.Descendants()
-                              if   isinstance( Element, app_elements.ClickableElement )
-                              if   getattr(    Element, 'id' ) == 'new-scan' ])[0]
-                              
-        if NewScanButton:
-            NewScanButton.Click()
-        else:
-            return False
+            PasswordField = ([  Element                            
+                                for  Element  in  WindowsPageRoot.Descendants()
+                                if   isinstance( Element, app_elements.EditableElement )
+                                if   getattr( Element, 'aria-label' ) == 'Password' 
+                                for  Ancestor in  Element.Ancestors()
+                                if   getattr( Ancestor, 'data-parent-id' ) == 'auth_method'
+                                if   'hide' not in getattr( Ancestor, 'class' ) ])[0]
+                            
+            UsernameField.SendKeys('Administrator')
+            PasswordField.SendKeys('admin')
 
-        WaitFor = { 'Tag' : 'div', 'class' : 'library' }
-        
-        if not self.LoadPage( PageName = "ScanTemplatesPage", WaitFor = WaitFor ):
-            print('Failed to load ScanTemplatesPage');
-            return False
-        
-        ScanTemplatesPageRoot  = self.ScanTemplatesPage.PageElementTree
-
-        Categories  = ([    Element
-                            for  Element in ScanTemplatesPageRoot.Descendants()
-                            if   hasattr( Element, 'data-category' )
-                            if   getattr( Element, 'class' ) == 'category-templates' ])
-        
-        for Category in Categories:
-        
-            CategoryName = getattr( Category, 'data-category' )
-            
-            print( f"Category: {CategoryName}" )
-            
-            CategoryItems = ([  ( Element, Child.Text )
-                                for  Element in Category.Descendants()
-                                for  Child in Element.Descendants()
-                                if   isinstance( Element, app_elements.ClickableElement )
-                                if   getattr( Element, 'data-category' ) == CategoryName
-                                if   ( { 'class' : 'banner' }, True ) not in Element
-                                if   getattr( Child, 'class' ) == 'title'
-                            ])
-            
-            
-            for CategoryElement, CategoryTitle in CategoryItems:
-            
-                print("Creating Scan for: " + CategoryTitle)
-                
-                if not ( self.CreateScan( CategoryElement, CategoryTitle )):
-                    return False
-                
-                time.sleep(3)
-                NewScanButton.Instance = None
-                NewScanButton.Click()
-                
-                
-                
-            
-        
-        return True
-        
-        
-        
+            return True
+    
+    
     
     def Test( self ):
          
@@ -333,39 +265,13 @@ class TenableApp( WebApp ):
             self.ExitApp()
             return False
             
-        
-        #if Result:
-        #    print(str(Result))
-        #    self.DumpResult( Result, UsernameField )
-        #else:
-        #    print("No Result")
-            
-        
-        
-        #UsernameField.SetAttribute("value", "Administrator")
-        #PasswordField.Click()
-        #PasswordField.SetAttribute("value", "admin")
             
         Pause("Test Complete")
         
         self.ExitApp()
         quit()
     
-        '''
-        LogonButton  = ([   Element
-                            for Element in RootNode.Descendants()
-                            if  Element.Name == 'button_0'
-                            if  getattr( Element.Parent, 'id' ) == 'start' 
-                            ])[0]
-                                
-        '''    
-        #Result = app_config.Driver.execute( Command.W3C_EXECUTE_SCRIPT,
-        #                                    {  'script' : Script,
-        #                                       'args'  : Args     })
-            
     
-
-
 
 
 if __name__ == '__main__':
@@ -375,76 +281,3 @@ if __name__ == '__main__':
 
 
 
-
-'''
-            Script = "$('#start').hide();"
-            Script = "return document.getElementById('start')"
-            Script = "return document.getElementById('start').nodeName"
-            Script = "return document.getElementById('start').nodeType"
-            Script = "return document.documentElement.firstChild"
-            Script = "return document.activeElement.nodeName"
-            Script = "return document.fonts"
-            Script = "return document.implementation"
-            Script = "return document.scripts"
-            Script = "return document.scripts[0].nodeName"
-            Script = "return document.scripts[0].firstChild"
-            Script = "return document.getElementById('start').getAttribute('id')"
-            Script = "return document.getElementById('start').childNodes[0].nodeValue"
-            Script = "return document.getElementById('start').innerHTML"
-            Script = "return document.getElementById('start').attributes"
-            Script = "return document.getElementById('start').id"
-            Script = "return document.getElementById('start').childNodes[0].nextSibling.nodeName"
-            Script = "return document.getElementById('start').childNodes[0].nextSibling.childNodes[0].replaceData('Foobar')"
-            Script = "return (document.getElementById('start').childNodes[0].nextSibling.childNodes[0].data = 'foobar')"
-            Script = "return (document.getElementById('start').childNodes[1].childNodes[0].data = 'foobar')"
-            Script = "return document.documentElement.querySelector('button').innerHTML;"
-            Script = "return document.getElementById('start').childNodes[0].nextSibling.innerHTML;"
-            Script = "return document.getElementById('start').childNodes[1].innerHTML;"
-            Script = "document.getElementById('start').childNodes[1].innerHTML = 'foobar';"            
-            Script = "return ( document.querySelector('button').innerHTML = 'HackerRank Test For Tenable' );"
-            Script = "return ( document.querySelector('button').setAttribute('style','display:none'))"
-            Script = "return document.querySelector('document.children[0].children[1].children[1].children[2].children[1]');"
-'''
-
-
-'''
-                #Script = ( Node.DOMPath + ".dispatchEvent(new MouseEvent('click', " +
-                #            "{ bubbles    : true, " +
-                #            "  cancelable : true, " +
-                #            "  view       : window, } ));" )
-
-                Script1 = ( "return (" + Node.DOMPath + ".dispatchEvent(new FocusEvent('focus', " +
-                            "{ bubbles    : true, " +
-                            "  cancelable : true, " +
-                            "  button     : 0, " +
-                            "  view       : window } )));" )
-
-                Script2 = ( "return (" + Node.DOMPath + ".dispatchEvent(new MouseEvent('click', " +
-                            "{ bubbles    : true, " +
-                            "  cancelable : true, " +
-                            "  button     : 0, " +
-                            "  view       : window  } )));" )
-                            
-                            
-                Script3 = ( "return (" + Node.DOMPath + ".dispatchEvent(new MouseEvent('mousedown', " +
-                            "{ bubbles    : true, " +
-                            "  cancelable : true, " +
-                            "  button     : 0, " +
-                            "  view       : window } )));" )
-                            
-
-                Script4 = ( Node.DOMPath + ".dispatchEvent(new MouseEvent('mouseup', " +
-                            "{ bubbles    : true, " +
-                            "  cancelable : true, " +
-                            "  button     : 0, " +
-                            "  view       : window } ));" )
-
-
-
-        # <section id="notifications">
-        #   <div role="alert" class="notification error" data-id="d7697b5b-784c-42eb-b638-f6eab0446efa" data-notification-id="">
-        #    <div class="notification-message">Error: Invalid Credentials</div>
-
-'''
-   
-   
